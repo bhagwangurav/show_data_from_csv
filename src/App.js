63a683388f5e8fc4fa49059352e4044f0f8f4a38
bhagwan <bhagwangurav15@gmail.com>
2021-06-17 18:15:29 +0530

    react get csv data from rails api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,105 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState, useEffect } from "react";
+import { MDBDataTable } from "mdbreact";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "bootstrap-css-only/css/bootstrap.min.css";
+import "mdbreact/dist/css/mdb.css";
+const App = () => {
+  const [csvData, setCsvData] = useState({});
+  useEffect(() => {
+    fetch("https://enigmatic-ocean-08412.herokuapp.com/question")
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setCsvData(data.response);
+      });
+  }, []);
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  console.log(csvData);
+  const data = {
+    columns: [
+      {
+        label: "Pri",
+        field: "pri",
+        sort: "asc",
+        width: 150,
+      },
+      {
+        label: "Question",
+        field: "name",
+        sort: "asc",
+        width: 270,
+      },
+      {
+        label: "Teaming Stages",
+        field: "teaming_stages_name",
+        sort: "asc",
+        width: 200,
+      },
+      {
+        label: "Appears(Day)",
+        field: "appears",
+        sort: "asc",
+        width: 100,
+      },
+      {
+        label: "Frequency",
+        field: "frequency",
+        sort: "asc",
+        width: 150,
+      },
+      {
+        label: "Type",
+        field: "q_type",
+        sort: "asc",
+        width: 100,
+      },
+      {
+        label: "Role",
+        field: "role_name",
+        sort: "asc",
+        width: 100,
+      },
+      {
+        label: "Required",
+        field: "required",
+        sort: "asc",
+        width: 100,
+      },
+      {
+        label: "Conditions",
+        field: "conditions",
+        sort: "asc",
+        width: 100,
+      },
+      {
+        label: "Mapping",
+        field: "mapping_name",
+        sort: "asc",
+        width: 100,
+      },
+    ],
+    rows: csvData,
+  };
+
+  if (!csvData.length) {
+    return null;
+  } else {
+    return (
+      csvData.length && (
+        <MDBDataTable
+          striped
+          bordered
+          small
+          hover
+          entriesOptions={[10]}
+          data={data}
+          searching={false}
+        />
+      )
+    );
+  }
+};
 
 export default App;
